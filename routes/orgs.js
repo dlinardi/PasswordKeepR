c6f525771f99bf489173601123367f6fcfcf92be
dlinardi <dlinardi98@gmail.com>
2020-11-30 09:48:06 +0000

    GET api/orgs/:id/sites/:sites_id

--- routes/orgs.js
+++ routes/orgs.js
@@ -64,6 +64,29 @@ module.exports = (db) => {
             .status(500)
             .json({ error: err.message });
         });
+    })
+    .get("/:id/sites/:sites_id", (req, res) => {
+
+      const queryString = `
+        SELECT organizations.id as org_id, sites.id as site_id, url, login_name, account_email, tags, created_date, deleted_date, is_active
+        FROM sites
+        JOIN organizations ON organizations.id = org_id
+        WHERE organizations.id = $1 AND sites.id = $2
+        GROUP BY sites.id, organizations.id;
+        `
+
+      const values = [`${req.params.id}`, `${req.params.sites_id}`];
+
+      db.query(queryString, values)
+        .then(data => {
+          const orgs = data.rows;
+          res.json({ orgs });
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
     });
   return router;
 };
