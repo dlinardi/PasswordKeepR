a5aeff3e0da1bb542e648dd6d21ac0b17cbc0c51
dlinardi <dlinardi98@gmail.com>
2020-12-01 15:26:54 +0000

    post route /

---
+++ routes/root.js
@@ -0,0 +1,45 @@
+/*
+ * All routes for Users are defined here
+ * Since this file is loaded in server.js into api/users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+const dbHelpers = require('../db/index');
+
+module.exports = (db) => {
+  router
+    .get("/", (req, res) => {
+      res.render("index");
+    })
+    .post("/new", (req, res) => {
+      const site = req.body;
+      dbHelpers.addSite(site)
+        .then(site => {
+          console.log(site);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    // .put("/edit", (req, res) => {
+
+    //   const { siteProperty, newValue, siteId } = req.body;
+    //   console.log(siteProperty, newValue, siteId);
+    //   dbHelpers.updateSite(siteProperty, newValue, siteId)
+    //     .then((siteProperty, newValue, siteId) => {
+    //       console.log(siteProperty, newValue, siteId);
+    //       console.log('success');
+    //       res.message('test');
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //     });
+    // })
+    // .delete("delete", (req, res) => {
+    //   console.log(req.body);
+    // });
+  return router;
+};
