0b147826d29288cc97f0729b2124038973935ed2
Charles P <charles.prystupa@gmail.com>
2020-12-02 14:33:46 +0000

    WiP: Clipboard Fix

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
 
 
-  $('footer').on('click', '.card-action', function (event) {
+  $(document).on('click', '.card-action', function (event) {
     // Targets .pwd Class of the Clicked urlInfo, will clip innerText
     console.log("THIS>>",$(this))
     const toClip = $(this).parent().find('.pwd')[0].innerText
