fc26b14624013d5285cee4bd7fdc30ab60555fbc
Charles P <charles.prystupa@gmail.com>
2020-12-02 04:07:50 +0000

    WiP Clipboard Function Targetting

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,14 +3,15 @@ $(document).ready(function () {
   console.log("DOC READY")
 
 
-  $('.card-action').on('click', function (event) {
-    event.preventDefault();
-    console.log($(this))
-    // Targets .pwd Class of the Clicked urlInfo, will clip innerText
-    // const toClip = $(this).children('.pwd')[0].innerText
-    // copyToClipboard(toClip)
-  });
 
-  loadSites(renderSites, 18);
+  $('footer').on('click', '.card-action', function (event) {
+    // Targets .pwd Class of the Clicked urlInfo, will clip innerText
+    console.log("THIS>>",$(this))
+    const toClip = $(this).parent().find('.pwd')[0].innerText
+    console.log(toClip)
+    copyToClipboard(toClip)
+  });
+// Org 18
+loadSites(renderSites, 18);
 
 });
